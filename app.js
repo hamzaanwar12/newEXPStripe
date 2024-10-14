7402feabcb5108b6c16950595bcb71b468d2bf71
Hamza Anwar <147822744+hamzaanwar12@users.noreply.github.com>
2024-10-14 10:47:54 +0500

    Changed

--- app.js
+++ app.js
@@ -50,6 +50,12 @@ app.post('/payment-sheet', async (req, res) => {
   }
 });
 
+
+
+app.get(('/', (req, res) => {
+  res.send('Hello, World!');
+}));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
